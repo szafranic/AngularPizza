5610101fbb5c8ef07513782749678ecdf6523ebd
szafranic <ryan.szafranic@gmail.com>
2022-11-09 21:07:19 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'src/menuitem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPizza';
+
+  chirpMenu : MenuItem[] = [
+  {item:"bbq chicken pizza",category:"pizzas",price:13.99},
+  {item:"pineapple pizza ",category:"pizzas",price:11.99},
+  {item:"fries",category:"snacks",price:3.99},
+  {item:"onion rings",category:"snacks",price:2.99},
+  {item:"ice cream sundae",category:"treats",price:4.49},
+  {item:"reeses cups",category:"treats",price:1.89}];
+
 }
+
+
